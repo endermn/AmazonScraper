eaae3c4c7238e9a4cd1639df789f98bab0fde211
gevla <viktorboyadjiev@gmail.com>
2023-08-22 19:21:59 +0200

    Update LinkScraper.py

--- LinkScraper.py
+++ LinkScraper.py
@@ -11,7 +11,7 @@ class Link_Scraper:
         try: 
             links = soup.find_all("a", attrs={'class':'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
         except:
-            return []
+            return set()
         links_list: set[str] = set()
 
         for link in links:
